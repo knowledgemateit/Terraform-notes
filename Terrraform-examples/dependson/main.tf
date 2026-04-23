371d11cfd44cc60f3f4e965bfa72e58a7e45966e
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-04-23 21:53:57 +0530

    Create main.tf

---
+++ Terrraform-examples/dependson/main.tf
@@ -0,0 +1,30 @@
+# Provider configuration
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "bharath-devops-test-2025"
+}
+
+resource "aws_s3_bucket_policy" "allow_access" {
+  bucket = aws_s3_bucket.logs.id
+
+  # THE EASY EXAMPLE:
+  # "Wait until the bucket is 100% finished before trying to attach this policy"
+  depends_on = [aws_s3_bucket.logs]
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::bharath-devops-test-2025/*"
+    }
+  ]
+}
+POLICY
+}

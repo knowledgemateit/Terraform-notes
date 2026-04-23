21330b7acb1f3c375be9d50fe7dd4c8a0f30d773
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-04-23 21:16:52 +0530

    Update main.tf

--- Terrraform-examples/s3backend/main.tf
+++ Terrraform-examples/s3backend/main.tf
@@ -8,9 +8,9 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-071226ecf16aa7d96"
-  instance_type          = "t2.micro"
-  key_name               = "tets22"
+  ami                    = "ami-098e39bafa7e7303d"
+  instance_type          = "t3.micro"
+  key_name               = "terraform_Server"
   monitoring             = true
   
   tags = {
@@ -21,7 +21,7 @@ module "ec2_instance" {
 }
 terraform {
   backend "s3" {
-    bucket         = "rajutraingingtest" # Replace with your S3 bucket name
+    bucket         = "my-unique-avan-name" # Replace with your S3 bucket name
     key            = "terraform.tfstate" # Key is the name of the state file in the bucket
     region         = "us-east-1" # Replace with your desired AWS region
     encrypt        = true
